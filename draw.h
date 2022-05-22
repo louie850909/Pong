9f5522d80f5c56287430759f7a285626012d8a12
HAL <HAL@WTHS20564>
2022-05-22 16:36:13 +0900

    プロジェクト ファイルを追加します。

---
+++ draw.h
@@ -0,0 +1,133 @@
+﻿#pragma once
+
+/*******************************************************************************
+* インクルードファイル
+*******************************************************************************/
+#include <stdio.h>
+#include <Windows.h>
+
+/*******************************************************************************
+* マクロ定義
+*******************************************************************************/
+#define WINDOW_WIDTH (80)
+#define WINDOW_HEIGHT (25)
+
+#define SPACE (0)
+#define STAR_MARK (1)
+#define BALL_MARK (2)
+#define X_MARK (3)
+
+/*******************************************************************************
+* 構造体定義
+*******************************************************************************/
+typedef struct rect
+{
+	int StartPoint_x;
+	int StartPoint_y;
+	int Width;
+	int Height;
+	int Type;
+} Rect;
+
+/*******************************************************************************
+* プロトタイプ宣言
+*******************************************************************************/
+void Draw(int* window, int* prewindow);
+void FillIn(int* window, Rect rect);
+void setCursorPosition(int x, int y);
+void Clear(int* window);
+
+/*******************************************************************************
+* グローバル変数
+*******************************************************************************/
+
+/*******************************************************************************
+ 関数名:		void Draw(int* Window_p)
+ 引数:		int* Window_p
+ 戻り値:		なし
+ 説明:		window中の内容を画面に表示する
+*******************************************************************************/
+void Draw(int* window, int* prewindow)
+{
+	for (int row = 0; row < WINDOW_HEIGHT; row++)
+	{
+		for (int column = 0; column < WINDOW_WIDTH; column++)
+		{
+			if (*(window + column + WINDOW_WIDTH * row) == *(prewindow + column + WINDOW_WIDTH * row))
+			{
+				continue;
+			}
+
+			setCursorPosition(column, row);
+			switch (*(window + column + WINDOW_WIDTH * row))
+			{
+			case SPACE:
+				printf(" ");
+				break;
+
+			case STAR_MARK:
+				printf("*");
+				break;
+
+			case BALL_MARK:
+				printf("O");
+				break;
+
+			case X_MARK:
+				printf("X");
+				break;
+			}
+
+			*(prewindow + column + WINDOW_WIDTH * row) = *(window + column + WINDOW_WIDTH * row);
+		}
+	}
+}
+
+/*******************************************************************************
+ 関数名:		void FillIn(int* Window_p, Rect rect)
+ 引数:		int* Window_p, Rect rect
+ 戻り値:		なし
+ 説明:		rectをconsole bufferに導入する
+*******************************************************************************/
+void FillIn(int* window, Rect rect)
+{
+	for (int row = rect.StartPoint_y; row < rect.StartPoint_y + rect.Height; row++)
+	{
+		for (int column = rect.StartPoint_x; column < rect.StartPoint_x + rect.Width; column++)
+		{
+			switch (rect.Type)
+			{
+			case STAR_MARK:
+				*(window + column + WINDOW_WIDTH * row) = STAR_MARK;
+				break;
+
+			case BALL_MARK:
+				*(window + column + WINDOW_WIDTH * row) = BALL_MARK;
+				break;
+
+			case X_MARK:
+				*(window + column + WINDOW_WIDTH * row) = X_MARK;
+				break;
+			}
+			
+		}
+	}
+}
+
+void setCursorPosition(int x, int y)
+{
+	static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD coord = { (SHORT)x, (SHORT)y };
+	SetConsoleCursorPosition(hOut, coord);
+}
+
+inline void Clear(int* window)
+{
+	for (int row = 0; row < WINDOW_HEIGHT; row++)
+	{
+		for (int column = 0; column < WINDOW_WIDTH; column++)
+		{
+			*(window + column + WINDOW_WIDTH * row) = 0;
+		}
+	}
+}
---
+++ main.cpp
@@ -0,0 +1,153 @@
+﻿/*******************************************************************************
+* タイトル:	メインプログラム
+* プログラム名:	main.cpp
+* 作成者:		林劭羲
+* 作成日:			2022/05/18
+*******************************************************************************/
+
+/*******************************************************************************
+* scanf のwarning防止
+*******************************************************************************/
+#define _CRT_SECURE_NO_WARNINGS
+
+/*******************************************************************************
+* インクルードファイル
+*******************************************************************************/
+#include <stdio.h>
+#include <conio.h>
+#include <Windows.h>
+#include <time.h>
+#include "draw.h"
+
+/*******************************************************************************
+* マクロ定義
+*******************************************************************************/
+#define KB_UP 72
+#define KB_DOWN 80
+#define KB_LEFT 75
+#define KB_RIGHT 77
+#define KB_ESCAPE 27
+
+/*******************************************************************************
+* 構造体定義
+*******************************************************************************/
+
+/*******************************************************************************
+* プロトタイプ宣言
+*******************************************************************************/
+
+
+/*******************************************************************************
+* グローバル変数
+*******************************************************************************/
+
+
+/*******************************************************************************
+ 関数名:	int main( void )
+ 引数:		void
+ 戻り値:	正常終了: int型の 0
+ 説明:		メイン関数
+*******************************************************************************/
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int Window[WINDOW_HEIGHT][WINDOW_WIDTH] = { 0 };
+
+	int preWindow[WINDOW_HEIGHT][WINDOW_WIDTH] = { 0 };
+
+	int key_code = 0;
+
+	int direction_x = 1;
+	int direction_y = 1;
+
+	Rect rect1 = { 0,1,1,4,STAR_MARK };
+	Rect background_up = { 0,0,80,1,X_MARK };
+	Rect background_down = { 0,24,80,1,X_MARK };
+	Rect background_left = { 0,0,1,25,X_MARK };
+	Rect background_right = { 79,0,1,25,X_MARK };
+	Rect ball = { rand() % 39 + 40,rand() % 23+1,1,1,BALL_MARK };
+	int direction1 = 1;
+
+	while (1)
+	{
+		key_code = 0;
+
+		if (_kbhit())
+			key_code = _getch();
+
+		switch (key_code)
+		{
+		case KB_RIGHT:
+			rect1.StartPoint_x += 1;
+			break;
+		case KB_LEFT:
+			rect1.StartPoint_x += -1;
+			break;
+		case KB_UP:
+			rect1.StartPoint_y += -1;
+			break;
+		case KB_DOWN:
+			rect1.StartPoint_y += 1;
+			break;
+		case KB_ESCAPE:
+			return 0;
+		}
+
+
+		Clear(*Window);
+
+		FillIn(*Window, rect1);
+		FillIn(*Window, ball);
+		FillIn(*Window, background_down);
+		FillIn(*Window, background_up);
+		//FillIn(*Window, background_left);
+		FillIn(*Window, background_right);
+
+		Draw(*Window, *preWindow);
+
+		
+
+		if (rect1.StartPoint_x > 78)
+		{
+			rect1.StartPoint_x = 78;
+		}
+		if (rect1.StartPoint_x < 0)
+		{
+			rect1.StartPoint_x = 0;
+		}
+		if (rect1.StartPoint_y > 20)
+		{
+			rect1.StartPoint_y = 20;
+		}
+		if (rect1.StartPoint_y < 1)
+		{
+			rect1.StartPoint_y = 1;
+		}
+
+		if (ball.StartPoint_x == rect1.StartPoint_x + 1 && ball.StartPoint_y >= rect1.StartPoint_y && ball.StartPoint_y <= rect1.StartPoint_y + rect1.Height)
+		{
+			direction_x *= -1;
+		}
+		if (ball.StartPoint_y == 1 || ball.StartPoint_y == 23)
+		{
+			direction_y *= -1;
+		}
+		if (ball.StartPoint_x == 78)
+		{
+			direction_x *= -1;
+		}
+		if (ball.StartPoint_x < 0)
+		{
+			return 0;
+		}
+
+
+		ball.StartPoint_x += 1 * direction_x;
+		ball.StartPoint_y += 1 * direction_y;
+
+		Sleep(33);
+	}
+
+	return 0;
+}
